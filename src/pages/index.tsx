9c59eca230494852be8a84460b608bcee386e22e
cmrib <ciceromirandar@gmail.com>
2022-03-02 13:59:22 -0300

    add static generation to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
 import styles from './home.module.scss';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import { stripe } from '../services/stripe';
 
 interface HomeProp {
@@ -34,7 +34,7 @@ export default function Home({ product }: HomeProp) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 
   // request product price from stipe api
   const price = await stripe.prices.retrieve('price_1KX2orCmFX9GTaallMK9cDZT')
@@ -50,6 +50,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
   }
 }
