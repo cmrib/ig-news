4997afc55201dc70d4d54b0767300b95fa1c62ef
cmrib <ciceromirandar@gmail.com>
2022-03-17 16:15:46 -0300

    add post preview logic

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -44,7 +44,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
 
     const { slug } = params;
 
-    if (!session.activeSubscription) {
+    if (!session?.activeSubscription) {
         return {
             redirect: {
                 destination: '/',
